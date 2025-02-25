825fd3b60e5ae77a9b3b0aecb62c612711460d15
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-02-26 00:06:16 +0500

    feat: change api

--- src/store/api/axios.ts
+++ src/store/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:9001/",
+    baseURL: "https://pincode-dev.ru/swagger/",
     withCredentials: true,
 });
