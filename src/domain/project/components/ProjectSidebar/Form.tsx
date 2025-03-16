52889630912dfe0c1cacc47e14664ded80f66af9
irkatyman <tymanira@mail.ru>
2025-03-16 21:38:50 +0500

    feat

---
+++ src/domain/project/components/ProjectSidebar/Form.tsx
@@ -0,0 +1,99 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Button,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    Input,
+    Form as InternalForm,
+} from "@pin-code/uikit.lib";
+import { useState } from "react";
+import { CreateProjectDto } from "@model/index";
+import { EmojiSelect } from "@components/EmojiSelect";
+
+type Props = {
+    defaultValues?: CreateProjectDto;
+    onSubmit: (data: CreateProjectDto) => void;
+    submitButtonText: string;
+    workspaceId: string;
+};
+
+const formSchema = z.object({
+    name: z.string().min(2, "Минимальная длина - 2").max(50, "Максимальная длина - 50"),
+    emoji: z.string().min(1, "Введите emoji"),
+    description: z.string().min(2, "Минимальная длина - 2").max(50, "Максимальная длина - 50"),
+});
+
+export const Form = ({ defaultValues, submitButtonText, onSubmit, workspaceId }: Props) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues,
+    });
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <InternalForm {...form}>
+            <form
+                onSubmit={form.handleSubmit((data) => onSubmit({ ...data, workspaceId }))}
+                className="grid gap-4 py-4"
+            >
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Название</FormLabel>
+                            <div className="grid gap-2" style={{ gridTemplateColumns: "44px 1fr" }}>
+                                <FormField
+                                    control={form.control}
+                                    name="emoji"
+                                    render={({ field }) => (
+                                        <FormControl>
+                                            <EmojiSelect
+                                                value={field.value}
+                                                handleChange={(value) => {
+                                                    field.onChange(value);
+                                                    setOpen(false);
+                                                }}
+                                                isError={Boolean(form.getFieldState("emoji").error)}
+                                                open={open}
+                                                setOpen={setOpen}
+                                            />
+                                        </FormControl>
+                                    )}
+                                />
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                            </div>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Описание</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="pt-4 grid">
+                    <Button type="submit">{submitButtonText}</Button>
+                </div>
+            </form>
+        </InternalForm>
+    );
+};
