ffd2a22896ddba8303e05dd00a482508798b8dc6
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-03-25 00:10:29 +0500

    Update axios.ts

--- src/store/api/axios.ts
+++ src/store/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://pincode-dev.ru/swagger/",
+    baseURL: "https://backend-swagger.pincode-infra.ru/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
