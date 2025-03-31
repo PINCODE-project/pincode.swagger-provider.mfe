b20768d20157ebaf6948f99f1f01acc07da08e5b
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-03-31 23:00:46 +0500

    fix: login

--- src/store/auth/logout.ts
+++ src/store/auth/logout.ts
@@ -1,15 +1,9 @@
 import { apiDomain, resetDomain } from "@store/api";
 import { sample } from "effector";
 
-export const logoutFx = apiDomain.effect();
 export const logout = apiDomain.event();
 
 sample({
     clock: logout,
-    target: logoutFx,
-});
-
-sample({
-    clock: logoutFx.done,
     target: resetDomain,
 });
